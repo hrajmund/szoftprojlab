47c7ae63212b2f91d3beb583be96aa92e0965746
Schweitzer András Attila <schweitzeraa16@gmail.com>
2024-05-10 17:50:06 +0200

    GraphComponent partially done

--- Logarlec/src/main/java/Grafikus/GraphComponent.java
+++ Logarlec/src/main/java/Grafikus/GraphComponent.java
@@ -3,17 +3,65 @@ package Grafikus;
 import Modell.CursedRoom;
 import Modell.*;
 import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.Edge;
+import org.graphstream.graph.implementations.SingleGraph;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class GraphComponent {
     
     protected Graph graph;
+
+    String style = "ui.style";
+    
+    GameManager gm;
+    
+    public GraphComponent(GameManager gameManager){
+        this.gm = gameManager;
+        graph = new SingleGraph("GameGraph");
+    }
     
     public Graph getGraph() {
         return graph;
     }
     
-    public void CurrStudentChanged(Student OLD, Student NEW){
+    public void CurrStudentChanged(Student NEW){
+        Set<Room> allNewEdges = new HashSet<>();
+        allNewEdges.addAll(NEW.getCurrentRoom().getOutgoingDoors());
+        allNewEdges.addAll(NEW.getCurrentRoom().getIncomingDoors());
+
+        ArrayList<Room> newOutgoing = new ArrayList<>(NEW.getCurrentRoom().getOutgoingDoors());
+        ArrayList<Room> newIncoming = new ArrayList<>(NEW.getCurrentRoom().getIncomingDoors());
         
+
+            
+        for(int i = 0; i < graph.getEdgeCount(); i++) {
+            Edge e = graph.getEdge(i);
+            Room relevantRoom = null;
+            if( allNewEdges.contains((Room) e.getNode0().getAttribute("room"))){
+                relevantRoom = (Room)e.getNode0().getAttribute("room");
+            }
+            if( allNewEdges.contains((Room) e.getNode1().getAttribute("room"))){
+                relevantRoom = (Room)e.getNode1().getAttribute("room");
+            }
+            if(relevantRoom != null){
+                if(newOutgoing.contains(relevantRoom) && newIncoming.contains(relevantRoom)){
+                    e.setAttribute(style, "edge.two_way");
+                }
+                else if(newOutgoing.contains(relevantRoom)){
+                    e.setAttribute(style, "edge.going");
+                }
+                else if(newIncoming.contains(relevantRoom)){
+                    e.setAttribute(style, "edge.coming");
+                }
+            }
+            else{
+                e.setAttribute(style, "edge.default");
+            }
+        }
     }
     
     public void CurseHappened(CursedRoom cr){
@@ -29,19 +77,87 @@ public class GraphComponent {
     }
     
     public void RoomGasUpdate(Room room){
-        
+        boolean hasStudent = false;
+        for(Person s : room.getPeople()){
+            if(gm.labyrinth.getStudents().contains(s)) {
+                hasStudent = true;
+                break;
+            }
+        }
+        Node n = graph.getNode(room.getName());
+        //gázos lett
+        if(room.getGas()){
+            if(hasStudent){
+                n.setAttribute(style, "node.student_gas");
+            }
+            else{
+                n.setAttribute(style, "node.gas");
+            }
+        }
+        //gáztalan lett
+        else{
+            if(hasStudent){
+                n.setAttribute(style, "node.student");
+            }
+            else{
+                n.setAttribute(style, "node.default");
+            }
+        }
     }
     
     public void StudentDied(Room r){
-        
+        Node n = graph.getNode(r.getName());
+        if(r.getGas()){
+            n.setAttribute(style, "node.gas");
+        }
+        else{
+            n.setAttribute(style, "node.default");
+        }
     }
     
     public void addNode(Room r){
-        
+        Node n = graph.addNode(r.getName());
+        n.setAttribute("ui.label", r.getName());
+        n.setAttribute("room", r);
+        if(r.getGas()){
+            n.setAttribute(style, "node.gas");
+        }
+        else{
+            n.setAttribute(style, "node.default");
+        }
     }
     
     public void addEdge(Room r1, Room r2){
-        
+        Node n1 = graph.getNode(r1.getName());
+        Node n2 = graph.getNode(r2.getName());
+        Edge e = graph.addEdge(r1.getName() + r2.getName(), n1, n2);
+    }
+    
+    public void addStudent(Student s){
+        Node n = graph.getNode(s.getCurrentRoom().getName());
+        if(s.getCurrentRoom().getGas()){
+            n.setAttribute(style, "node.student_gas");
+        }
+        else{
+            n.setAttribute(style, "node.student");
+        }
+    }
+    
+    public void studentMoved(Room oldR, Room newR){
+        Node oldN = graph.getNode(oldR.getName());
+        Node newN = graph.getNode(newR.getName());
+        if(oldR.getGas()){
+            oldN.setAttribute(style, "node.gas");
+        }
+        else{
+            oldN.setAttribute(style, "node.default");
+        }
+        if(newR.getGas()){
+            newN.setAttribute(style, "node.student_gas");
+        }
+        else{
+            newN.setAttribute(style, "node.student");
+        }
     }
     
     
